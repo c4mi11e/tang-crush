18b5104c39175b59ebfd7fed7f9fcbad1df27205
c4mi11e <86762694+c4mi11e@users.noreply.github.com>
2021-07-01 13:16:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const CONFIG = {
     btnNo: 'Cậu mơ à :33',
     question:'Nói cho tớ lí do cậu thích tớ đi :vvvv',
     btnReply: 'Gửi cho tớ <3',
-    btnReplyno : 'Thui ngại lém :<<<<', 
+    btnReply : 'Thui ngại lém :<<<<', 
     reply: 'Vì cậu xinh gái vl >:((((',
     mess: 'Awwww >< tớ bíc mà yêu cậu moah moah :33',
-    messDesc: 'Nghiện mà còn ngại à ghéc ghê >:((((',
+    messDesc: 'Nghiện mà còn ngại à ghéc ghê mau ib người ta đi kìa :<<<',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //https://www.messenger.com/t/100016372809934
 }
